4294413d2b17b6a6f9db6cc5ac80d2529cc60e3d
Mila48Drokina <Drokina.lyudmila48@yandex.ru>
2024-05-13 23:33:04 +0300

    create.sql added

--- create.sql
+++ create.sql
@@ -1,19 +1,53 @@
+create table if not exists Musical_genre (
+id serial primary key,
+name varchar (50) not null unique);
 
-select name, duration from Tracks 
-ORDER by duration desc 
-limit 1;
+create table if not exists Artists (
+id serial primary key,
 
-select name from tracks
-where duration >='210';
+name varchar (50)  not null
+);
 
-select name from collection
-where year_release between '2018' and'2020';
+create table if not exists Genre_Artists (
+Artist_id integer references Artists(id),
+musical_genre_id integer references Musical_genre(id),
+constraint Genre_Artists_id primary key (Artist_id, Musical_genre_id)
+);
 
-select name from artists
-where name not like '% %';
 
-select name from tracks
-where tracks.name contains 'my' or  tracks.name like 'Moi';
+
+
+create table if not exists Albums (
+id serial primary key,
+name varchar (50) not null,
+Year_of_release integer CHECK (Year_of_release <= 2024 )
+);
+
+create table if not exists AlbumsArtists (
+album_id integer references Albums(id),
+Artist_id integer references Artists(id),
+constraint AlbumsArtists_id primary key (Album_id, Artist_id));
+
+
+
+create table if not exists Tracks (
+id serial primary key,
+name varchar (50) not null,
+duration integer not null, check(duration >= '33'),
+Album_id integer references Albums(id)
+);
+
+
+
+create table if not exists Collection (
+id serial primary key,
+name varchar (50) not null,
+Year_release integer not null);
+
+create table if not exists Track_Collection (
+track_id integer references Tracks(id),
+collection_id integer references Collection(id),
+constraint Track_Collection_id primary key (track_id, Collection_id));
 
 
 
